dace3dd0cc17b8b77806b3156947147db679ef5d
Nsks123 <nallamsunilkumar5@gmail.com>
2024-03-27 22:35:33 +0530

    [Sunil] Add:Implemented Functionality BookStore

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,226 @@
+﻿using Common_BookStore_Layer.RequestModel;
+using Common_BookStore_Layer.ResponseModel;
+using Manager_BookStore_Layer.Interfaces;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Repository_BookStore_Layer.BookStoreContext;
+using Repository_BookStore_Layer.BookStoreEntity;
+using System;
+using System.Collections.Generic;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+
+        private readonly IBookManager bookManager;
+        public BookController(IBookManager bookManager)
+        {
+            this.bookManager = bookManager;
+        }
+        [HttpPost]
+        [Route("AddBook")]
+        public ActionResult AddBook(CreateBookModel model)
+        {
+            try
+            {
+                var response = bookManager.CreateBook(model);
+                if (response != null)
+                {
+                    return Ok(new ResModel<BookEntity> { Success = true, Message = "Create Book Success", Data = response });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<BookEntity> { Success = false, Message = " Create Book Failed", Data = response });
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResModel<BookEntity> { Success = false, Message = " Create Book Failed", Data = null });
+
+            }
+        }
+        [HttpGet]
+        [Route("GetAllBooks")]
+        public ActionResult GetAllBooks()
+        {
+            try
+            {
+                var response = bookManager.GetAllBook();
+                if(response !=null)
+                {
+
+                    return Ok(new ResModel<List<BookEntity>> { Success = true, Message = "Getting All Books Success", Data = response });
+
+                }
+                else
+                {
+
+                    return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Getting All Books Failed", Data = response });
+                }
+            }
+            catch(Exception ex)
+            {
+
+                return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Getting All Books Failed", Data = null });
+            }
+        }
+        [HttpGet]
+        [Route("GetBookById")]
+        public ActionResult GetBook(int id)
+        {
+            try
+            {
+                var response = bookManager.GetBookId(id);
+                if (response != null)
+                {
+
+                    return Ok(new ResModel<List<BookEntity>> { Success = true, Message = "Get Book Success", Data = response });
+
+                }
+                else
+                {
+
+                    return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Get Book Failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Get Book Failed", Data = null });
+            }
+        }
+        [HttpGet]
+        [Route("GetBookBySearch")]
+        public ActionResult GetBySearch(string author,string bookname)
+        {
+            try
+            {
+                var response = bookManager.GetBySearch(author,bookname);
+                if (response != null)
+                {
+
+                    return Ok(new ResModel<List<BookEntity>> { Success = true, Message = "Get Book Success", Data = response });
+
+                }
+                else
+                {
+
+                    return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Get Book Failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Get Book Failed", Data = null });
+            }
+        }
+        [HttpGet]
+        [Route("SortPriceInAsc")]
+        public ActionResult SortPriceInAsc()
+        {
+            try
+            {
+                var response = bookManager.SortByPrice();
+                if (response != null)
+                {
+
+                    return Ok(new ResModel<List<BookEntity>> { Success = true, Message = "Sorted Price Success", Data = response });
+
+                }
+                else
+                {
+
+                    return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Price Failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Price Failed", Data = null });
+            }
+        }
+        [HttpGet]
+        [Route("SortPriceInDes")]
+        public ActionResult SortPriceInDes()
+        {
+            try
+            {
+                var response = bookManager.SortByPriceDes();
+                if (response != null)
+                {
+
+                    return Ok(new ResModel<List<BookEntity>> { Success = true, Message = "Sorted Price Descending Success", Data = response });
+
+                }
+                else
+                {
+
+                    return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Price Descending Failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Price Descending Failed", Data = null });
+            }
+        }
+        [HttpGet]
+        [Route("SortArrivalInAsc")]
+        public ActionResult SortArrivalInAsc()
+        {
+            try
+            {
+                var response = bookManager.SortByArrivalAsc();
+                if (response != null)
+                {
+
+                    return Ok(new ResModel<List<BookEntity>> { Success = true, Message = "Sorted Arrival Ascending Success", Data = response });
+
+                }
+                else
+                {
+
+                    return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Arrival Ascending Failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Arrival Ascending Failed", Data = null });
+            }
+        }
+        [HttpGet]
+        [Route("SortArrivalInDes")]
+        public ActionResult SortArrivalInDes()
+        {
+            try
+            {
+                var response = bookManager.SortByArrivalDes();
+                if (response != null)
+                {
+
+                    return Ok(new ResModel<List<BookEntity>> { Success = true, Message = "Sorted Arrival Descending Success", Data = response });
+
+                }
+                else
+                {
+
+                    return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Arrival Descending Failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new ResModel<List<BookEntity>> { Success = false, Message = " Sorted Arrival Descending Failed", Data = null });
+            }
+        }
+
+
+    }
+}
---
+++ BookStore/Controllers/UserController.cs
@@ -0,0 +1,134 @@
+﻿using Common_BookStore_Layer;
+using Common_BookStore_Layer.RequestModel;
+using Common_BookStore_Layer.ResponseModel;
+using Manager_BookStore_Layer.Interfaces;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Repository_BookStore_Layer.BookStoreContext;
+using Repository_BookStore_Layer.BookStoreEntity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserManager userManager;
+        private readonly IBus bus;
+        private readonly UserContext userContext;
+        private readonly IDistributedCache _cache;
+        public UserController(IUserManager userManager,IBus bus,UserContext userContext,IDistributedCache _cache)
+        {
+            this.userManager = userManager;
+            this.bus = bus;
+            this.userContext = userContext;
+            this._cache=_cache;
+        }
+        [HttpPost]
+        [Route("UserRegistration")]
+        public ActionResult UserRegister(UserRegModel model)
+        {
+            try
+            {
+                var response = userManager.UserRegistration(model);
+                if (response != null)
+                {
+                    return Ok(new ResModel<UserEntity> { Success = true, Message = " User Registration Success", Data = response });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<UserEntity> { Success = false, Message = " User Registration Failed", Data = response });
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new ResModel<UserEntity> { Success = false, Message = " User Registration Failed", Data = null });
+
+            }
+        }
+        [HttpPost]
+        [Route("UserLogin")]
+        public ActionResult UserLogin(UserLoginModel model)
+        {
+            try
+            {
+                var response = userManager.UserLogin(model);
+                if(response != null)
+                {
+                    return Ok(new ResModel<string> { Success = true, Message = "User Login Successful", Data = response });
+                }
+                else
+                {
+                    return BadRequest(new ResModel<string> { Success = false, Message = "User Login Failed", Data = response });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResModel<string> { Success = false, Message = "User Login Failed", Data = null });
+            }
+        }
+        [HttpPost]
+        [Route("Forgot")]
+        public async Task<ActionResult> ForgotPassword(string Email)
+        {
+            try
+            {
+                if (userManager.CheckEmail(Email))
+                {
+                    SendEmail mail = new SendEmail();
+                    ForgotPasswordModel forgotPasswordModel = userManager.ForgetPass(Email);
+                    var checkmail = userContext.UserTable.FirstOrDefault(x => x.Emailid == Email);
+                    mail.SendMail(Email, forgotPasswordModel.token);
+                    Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                    var endPoint = await bus.GetSendEndpoint(uri);
+                    await endPoint.Send(forgotPasswordModel);
+                    return Ok(new ResModel<string> { Success = true, Message = "Mail sent successfully", Data = forgotPasswordModel.token });
+
+                }
+                else
+                {
+                    return BadRequest(new ResModel<string> { Success = false, Message = "Email Does Not Exit", Data = Email });
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new ResModel<string> { Success = false, Message = "Exception: " + ex.Message, Data = null });
+            }
+
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("Reset")]
+        public ActionResult ResetPassword(ResetPasswordModel reset)
+        {
+            try
+            {
+                string Email = User.FindFirst("Emailid").Value;
+                if (userManager.ResetPassword(Email, reset))
+                {
+                    return Ok(new ResModel<bool> { Success = true, Message = "Password Changed", Data = true });
+
+                }
+                else
+                {
+                    return BadRequest(new ResModel<bool> { Success = false, Message = "Password Not Changed", Data = false });
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
