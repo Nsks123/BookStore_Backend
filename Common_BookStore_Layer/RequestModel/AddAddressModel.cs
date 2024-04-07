50ae92380e2360be1c1e57a8857e4b03800e3e1c
Nsks123 <nallamsunilkumar5@gmail.com>
2024-04-07 20:49:44 +0530

    [sunil] Add:Implemented DeleteWishlist

--- Common_BookStore_Layer/RequestModel/AddAddressModel.cs
+++ Common_BookStore_Layer/RequestModel/AddAddressModel.cs
@@ -10,7 +10,9 @@ namespace Common_BookStore_Layer.RequestModel
         public string FullName { get; set; }
         public string MobileNumber { get; set; }
         public string Address { get; set; }
+
         public string City { get; set; }
+
         public string State { get; set; }
 
         public string label { get; set; }
