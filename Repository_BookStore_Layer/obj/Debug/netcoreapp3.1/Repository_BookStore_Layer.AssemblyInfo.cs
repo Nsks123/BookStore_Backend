59f0de69ec0668ae27905160d3e119e365102746
Nsks123 <nallamsunilkumar5@gmail.com>
2024-04-07 18:52:17 +0530

    [sunil] Add:Implemented Update Cart

--- Repository_BookStore_Layer/obj/Debug/netcoreapp3.1/Repository_BookStore_Layer.AssemblyInfo.cs
+++ Repository_BookStore_Layer/obj/Debug/netcoreapp3.1/Repository_BookStore_Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository_BookStore_Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3785c65eb25ffc470be72d63814215ba1debc5ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54d9619095a3ac77339adc424864d952bb94f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository_BookStore_Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository_BookStore_Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
