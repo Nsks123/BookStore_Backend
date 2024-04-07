393c4d2025ce56b4c99c4a710f027822070fa09d
Nsks123 <nallamsunilkumar5@gmail.com>
2024-04-07 20:43:21 +0530

    [sunil] Add:Implemented AddToWishlist

--- BookStore/Controllers/WishListController.cs
+++ BookStore/Controllers/WishListController.cs
@@ -19,6 +19,7 @@ namespace BookStore.Controllers
         {
             this.wishlistManager = wishlistManager;
         }
+        //Add to Wishlist
         [Authorize]
         [HttpPost]
         [Route("AddToWishList")]
